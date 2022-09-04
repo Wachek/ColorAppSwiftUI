ce48f0e19b06d29b84cd04780775e941f544328f
Wachek <wachek7@gmail.com>
2022-09-04 21:48:49 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Вячеслав Турчак on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
