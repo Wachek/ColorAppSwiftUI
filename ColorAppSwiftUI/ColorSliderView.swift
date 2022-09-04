11b96e3c2b5196a155ee092bb107ea50d42dbffb
Wachek <wachek7@gmail.com>
2022-09-04 22:02:39 +0300

    Initial commit

---
+++ ColorAppSwiftUI/ColorSliderView.swift
@@ -0,0 +1,21 @@
+//
+//  ColorSliderView.swift
+//  ColorAppSwiftUI
+//
+//  Created by Вячеслав Турчак on 04.09.2022.
+//
+
+import SwiftUI
+
+struct ColorSliderView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSliderView()
+    }
+}
