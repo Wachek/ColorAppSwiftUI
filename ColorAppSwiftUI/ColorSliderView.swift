66e44bc0cc8d579bb5f26ea22517a66e72491775
Wachek <wachek7@gmail.com>
2022-09-08 22:47:18 +0300

    added color view

--- ColorAppSwiftUI/ColorSliderView.swift
+++ ColorAppSwiftUI/ColorSliderView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ColorSliderView: View {
-    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
---
+++ ColorView.swift
@@ -0,0 +1,28 @@
+//
+//  ColorView.swift
+//  ColorAppSwiftUI
+//
+//  Created by Вячеслав Турчак on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    let color: CGColor
+    
+    var body: some View {
+        Color(color)
+            .frame(height: 150)
+            .cornerRadius(30)
+            .overlay(RoundedRectangle(cornerRadius: 30)
+                .stroke(Color.blue, lineWidth: 5)
+            )
+            .padding()
+    }
+}
+
+struct ColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorView(color: CGColor(red: 255, green: 165, blue: 0, alpha: 1))
+    }
+}
